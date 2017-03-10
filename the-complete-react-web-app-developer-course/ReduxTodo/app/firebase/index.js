45ca019f025f1bcf24cb53716576a4c4bbca0cb1
Ha Manh Do <hadm.haui@gmail.com>
2017-03-10 16:32:36 +0700

    ENV dynamic loading from custom files

--- the-complete-react-web-app-developer-course/ReduxTodo/app/firebase/index.js
+++ the-complete-react-web-app-developer-course/ReduxTodo/app/firebase/index.js
@@ -2,11 +2,11 @@ import firebase from 'firebase';
 
 try {
 	var config = {
-	    apiKey: "AIzaSyAy89HkCC1NxaJz_HjT921kNYM9FOblKCk",
-	    authDomain: "mead-todo-app-9ab49.firebaseapp.com",
-	    databaseURL: "https://mead-todo-app-9ab49.firebaseio.com",
-	    storageBucket: "mead-todo-app-9ab49.appspot.com",
-	    messagingSenderId: "1023950189485"
+	    apiKey: process.env.API_KEY,
+	    authDomain: process.env.AUTH_DOMAIN,
+	    databaseURL: process.env.DATABASE_URL,
+	    storageBucket: process.env.STORAGE_BUCKET,
+	    messagingSenderId: process.env.MESSAGING_SENDER_ID
 	  };
 	firebase.initializeApp(config);
 } catch (e) {
--- the-complete-react-web-app-developer-course/ReduxTodo/webpack.config.js
+++ the-complete-react-web-app-developer-course/ReduxTodo/webpack.config.js
@@ -1,9 +1,16 @@
 var webpack = require('webpack');
 var path = require('path');
+var envFile = require('node-env-file');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 // cmd: set NODE_ENV=production // will uglify js and reduce huge amount of bundle's file size
 // cmd: webpack -w
+
+try {
+  envFile(path.join(__dirname, 'config/' + process.env.NODE_ENV + '.env'));
+} catch (e) {
+
+}
 
 module.exports = {
   entry: [
@@ -26,7 +33,12 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify('production')
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        API_KEY: JSON.stringify(process.env.API_KEY),
+        AUTH_DOMAIN: JSON.stringify(process.env.AUTH_DOMAIN),
+        DATABASE_URL: JSON.stringify(process.env.DATABASE_URL),
+        STORAGE_BUCKET: JSON.stringify(process.env.STORAGE_BUCKET),
+        MESSAGING_SENDER_ID: JSON.stringify(process.env.MESSAGING_SENDER_ID)
       }
     })
   ],
@@ -67,5 +79,5 @@ module.exports = {
       path.resolve(__dirname, './node_modules/foundation-sites/scss')
     ]
   },
-  devtool: process.env.NODE_ENV === 'production' ? undefined : 'cheap-module-eval-source-map'
+  devtool: process.env.NODE_ENV === 'development' ? undefined : 'cheap-module-eval-source-map'
 };
